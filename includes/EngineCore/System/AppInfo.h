ba5f35a3904185fb049f0cd35ee98f4c3d0c28cf
lemon857 <lemon857.an@gmail.com>
2024-01-07 20:10:36 +0500

    Add icon

---
+++ includes/EngineCore/System/AppInfo.h
@@ -0,0 +1,11 @@
+#define APPINFO_PATH_TO_ICON "../res/icon.ico"
+
+#define APPINFO_FILE_VERSION 0,0,6,0
+#define APPINFO_PRODUCT_VERSION 0,0,6,0
+
+#define APPINFO_COMPANY_NAME "CoolsGamesDevelopers"
+#define APPINFO_LEGAL_COPYRIGHT "© CoolsGamesDevelopers"
+#define APPINFO_FILE_DESCRIPTION "Game"
+#define APPINFO_INTERNAL_NAME "Tower_defense"
+#define APPINFO_ORIGINAL_FILE_NAME "Tower defense"
+#define APPINFO_PRODUCT_NAME "Tower defense"
